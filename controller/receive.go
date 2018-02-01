34bf11eecf2b9c6ff8e7759b8c9e1585f163e494
makki0205 <llxo2_5oxll@icloud.com>
2018-02-02 01:26:30 +0900

    .

--- controller/receive.go
+++ controller/receive.go
@@ -3,38 +3,27 @@ package controller
 import (
 	"net/http"
 
-	"github.com/HAL-RO-Developer/iot_server/controller/validation"
+	"fmt"
+
 	"github.com/HAL-RO-Developer/iot_server/model"
 	"github.com/gin-gonic/gin"
 )
 
 func DeviceReceive(c *gin.Context) {
+	device_id := c.Param("id")
+	fmt.Println(device_id)
 	// デバイスへの命令検索
-	req, ok := validation.SearchMyFunction(c)
+	ok := model.ExistDeviceById(device_id)
 	if !ok {
-		return
-	}
-
-	// デバイスの登録チェック(未登録時エラーを返す)
-	ret := model.ExistDeviceByIam(req.DeviceID, "")
-	if ret {
-		c.JSON(http.StatusForbidden, gin.H{
-			"err": "デバイスが登録されていません。",
-		})
-		return
-	}
-
-	// デバイスIDチェック
-	res := model.ExistDeviceByIam(req.DeviceID, req.MacAddr)
-	if !res {
 		c.JSON(http.StatusBadRequest, gin.H{
-			"err": "デバイスIDが不正です。",
+			"err": "デバイスIDが登録されていません",
 		})
 		return
 	}
-
 	// 命令取得
-	value := model.GetTaskInfo(req.DeviceID)
-
+	value := model.GetTaskInfo(device_id)
+	if value == nil {
+		c.JSON(http.StatusOK, 0)
+	}
 	c.JSON(http.StatusOK, value[0].Args[0])
 }
